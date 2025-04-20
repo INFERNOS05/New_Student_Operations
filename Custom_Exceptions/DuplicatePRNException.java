747601369771971ab8f89647b0474f85a9dccbcd
sehajdeep <155955772+INFERNOS05@users.noreply.github.com>
2025-04-21 01:36:35 +0530

    Update DuplicatePRNException.java

--- Custom_Exceptions/DuplicatePRNException.java
+++ Custom_Exceptions/DuplicatePRNException.java
@@ -1,1 +1,16 @@
+/**
+ * Exception thrown when attempting to add a student whose PRN already exists in the system.
+ * PRN (Permanent Registration Number) must be unique for each student.
+ */
+public class DuplicatePRNException extends Exception {
+
+    /**
+     * Constructs a new DuplicatePRNException with a detailed error message.
+     *
+     * @param prn The PRN that caused the duplication error.
+     */
+    public DuplicatePRNException(long prn) {
+        super("Duplicate PRN detected: " + prn + ". Each student must have a unique PRN.");
+    }
+}
 
