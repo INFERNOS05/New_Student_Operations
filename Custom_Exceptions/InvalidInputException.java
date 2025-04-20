d0a350336fe71bb21cb0d7e8e70aa3022d8639f8
sehajdeep <155955772+INFERNOS05@users.noreply.github.com>
2025-04-21 00:24:57 +0530

    Update InvalidInputException.java

--- Custom_Exceptions/InvalidInputException.java
+++ Custom_Exceptions/InvalidInputException.java
@@ -1,1 +1,19 @@
+package CustomExceptions;
+
+/**
+ * Custom exception class to handle invalid input scenarios.
+ * This class extends RuntimeException, so it is an unchecked exception.
+ */
+public class InvalidInputException extends RuntimeException {
+
+    /**
+     * Constructor for InvalidInputException.
+     *
+     * @param message The error message to be passed when the exception is thrown.
+     */
+    public InvalidInputException(String message) {
+        // Pass the message to the parent (RuntimeException) constructor
+        super(message);
+    }
+}
 
